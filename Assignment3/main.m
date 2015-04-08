3ea2e18f3c84e634bd4908e8bc511b8e0a052f9c
Bianca Binstock <bianca.pr@gmail.com>
2015-04-07 21:52:02 -0400

    question1 until loops- will create a new file to finish question 1... hope this works!

--- Assignment3/main.m
+++ Assignment3/main.m
@@ -62,6 +62,22 @@ int main(int argc, const char * argv[]) {
         a && b  Logical and
         a || b  Logical or  */
         
+        // Switch statements (only work with integral types)
+        switch (modelYear) {
+            case 1987:
+                NSLog(@"Your car is from 1987.");
+                break;
+            case 1988:
+                NSLog(@"Your car is from 1988.");
+                break;
+            case 1989:
+            case 1990:
+                NSLog(@"Your car is from 1989 or 1990.");
+                break;
+            default:
+                NSLog(@"I have no idea when your car was made.");
+                break;
+        }
         
         
     
