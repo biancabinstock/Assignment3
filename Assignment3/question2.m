e0e16a15fddc5cf0f5c56ccaaace08f8caedd376
Bianca Binstock <bianca.pr@gmail.com>
2015-04-07 22:51:17 -0400

    finished question 2 committing

---
+++ Assignment3/question2.m
@@ -0,0 +1,17 @@
+//
+//  question2.m
+//  Assignment3
+//
+//  Created by Bianca Binstock on 2015-04-07.
+//  Copyright (c) 2015 Bianca Binstock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+int main4(int argc, const char * argv[]) {
+    @autoreleasepool {
+       
+        NSLog(@"This is my second day at bitmaker labs");
+        
+    }
+    return 0;
+}
